032a798ce50c8c24d90737d5cc0a6d9f7f21593c
cgp2409 <cristhian.guzman@udea.edu.co>
2023-05-24 19:38:53 -0500

    correccion

--- tablero_palermo.py
+++ tablero_palermo.py
@@ -2,10 +2,8 @@
 #IMPORTE DE LIBRERIAS
 #---------------------------------------------------
 import pandas as pd
-import json
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 import gspread
 import plotly.graph_objects as go
